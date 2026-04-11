388d4820f4f03efde3ed282bbe7d93df238b824d
trezpka <trezpka@gmail.com>
2026-04-12 01:11:12 +0300

    Delete jest.config.js

--- jest.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  preset: 'ts-jest',
-  testEnvironment: 'jsdom',
-  moduleNameMapper: {
-    '^@/(.*)$': '<rootDir>/$1'
-  },
-  setupFilesAfterEnv: ['@testing-library/jest-dom']
-};
