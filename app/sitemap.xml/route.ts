9081a66f10d283f4fb2250d40513dd813943192b
trezpka <trezpka@gmail.com>
2026-04-14 00:02:16 +0300

    Update route.ts

--- app/sitemap.xml/route.ts
+++ app/sitemap.xml/route.ts
@@ -20,15 +20,13 @@ export async function GET() {
   const xml = `<?xml version="1.0" encoding="UTF-8"?>
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
 ${allUrls
-  .map(
-    (path) => `
+  .map((path) => {
+    return `
   <url>
     <loc>${baseUrl}${path}</loc>
     <lastmod>${new Date().toISOString()}</lastmod>
-    <changefreq>monthly</changefreq>
-    <priority>${path === "" ? "1.0" : "0.8"}</priority>
-  </url>`
-  )
+  </url>`;
+  })
   .join("")}
 </urlset>`;
 
