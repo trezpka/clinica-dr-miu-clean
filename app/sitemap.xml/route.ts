3dcc6ef3306cb4e51f1a228f9a37d84e4989cdb4
trezpka <trezpka@gmail.com>
2026-04-13 22:51:43 +0300

    Update route.ts

--- app/sitemap.xml/route.ts
+++ app/sitemap.xml/route.ts
@@ -1,25 +1,34 @@
-export async function GET() {
-  const baseUrl = "https://www.clinicadrmiu.ro";
+import { clinic } from "@/lib/clinic";
+import { servicePages } from "@/lib/pages-data";
+import { blogPosts } from "@/lib/blog-data";
 
-  const urls = [
+export async function GET() {
+  const baseUrl = clinic.domain;
+
+  const staticPages = [
     "",
     "/despre-dr-miu",
     "/contact",
     "/blog",
   ];
 
+  const serviceUrls = servicePages.map((page) => page.path);
+  const blogUrls = blogPosts.map((post) => `/blog/${post.slug}`);
+
+  const allUrls = [...staticPages, ...serviceUrls, ...blogUrls];
+
   const xml = `<?xml version="1.0" encoding="UTF-8"?>
-  <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-    ${urls
-      .map(
-        (path) => `
-      <url>
-        <loc>${baseUrl}${path}</loc>
-        <lastmod>${new Date().toISOString()}</lastmod>
-      </url>`
-      )
-      .join("")}
-  </urlset>`;
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+${allUrls
+  .map(
+    (path) => `
+  <url>
+    <loc>${baseUrl}${path}</loc>
+    <lastmod>${new Date().toISOString()}</lastmod>
+  </url>`
+  )
+  .join("")}
+</urlset>`;
 
   return new Response(xml, {
     headers: {
